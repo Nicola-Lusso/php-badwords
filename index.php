500d97c9b160cf994994f930333a3e1513bbb015
Nicola <nicolusso@gmail.com>
2021-02-24 15:29:37 +0100

    Finish

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <?php
   $paragrafo = 'Oggi il sole splende per ';
   $badWord = $_GET['badWord'];
+  $newParagrafo = $paragrafo . $badWord;
 ?>
 
 <!DOCTYPE html>
@@ -12,11 +13,11 @@
   </head>
   <body>
 
-    <p> <?php echo $paragrafo ; ?> </p>
-    <p> <?php echo $badWord ; ?></p>
-    <p><?php $badWord = '***'; echo $badWord; ?></p>
-    <p> <?php echo $paragrafo . $badWord; ?> </p>
-  ​
+    <p> $paragrafo iniziale <?php echo $paragrafo ; ?> </p>
+    <p> $badWord scelta = <?php echo $badWord ; ?></p>
+    <p> $badWord modificata = <?php $badWord = '***'; echo $badWord; ?></p>
+    <p> $newParagrafo = <?php echo $paragrafo . $badWord; ?> </p>
+  ​  <p> La lunghezza totale del paragrafo è : <?php echo $lunghezzaTot = strlen($newParagrafo); ?></p>
 
 
 
